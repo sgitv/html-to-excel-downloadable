885ad7776a928aef011d915488c4bfc9e9c68f8c
sid1511996 <siddartha1511996@gmail.com>
2016-02-01 01:02:30 +0530

    html and js file separated

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>faker</title>
+	<link rel="stylesheet" type="text/css" href="bower_components\bootstrap\dist\css\bootstrap.css">
+</head>
+<body class="container">
+<table id="tbl1" border="1" >
+<tr>
+    <td>Product</td>
+    <td>Price</td>
+    <td>Available</td>
+    <td>Count</td>
+    </tr>
+<tr>
+    <td>Bred</td>
+    <td>7</td>
+    <td>8</td>
+    <td>9</td>
+</tr>
+<tr>
+    <td>Butter</td>
+    <td>14</td>
+    <td>15</td>
+    <td >16</td>
+</tr>
+</table>
+<button onclick="downloadAsExcel()">Export to Excel</button>
+<script src="bower_components/bootstrap/dist/js/download.js"></script>
+</body>
+</html>
